9f5eb55c247c0f4cc75a4e3eeb2b6c96bf581208
fridlaki <wilfriduslaki@gmail.com>
2019-07-21 21:05:58 +0700

    first commite ci with react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (    
+      <div id="container">
+        <h1>Welcome to CodeIgniter!</h1>
+        <div id="body">
+          <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+          <p>If you would like to edit this page you'll find it located at:</p>
+          <code>application/views/welcome_message.php</code>
+          <p>The corresponding controller for this page is found at:</p>
+          <code>application/controllers/Welcome.php</code>
+          <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+        </div>
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (    
+//     <div id="container">
+//       <h1>Welcome to CodeIgniter!</h1>
+//       <div id="body">
+//         <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+//         <p>If you would like to edit this page you'll find it located at:</p>
+//         <code>application/views/welcome_message.php</code>
+//         <p>The corresponding controller for this page is found at:</p>
+//         <code>application/controllers/Welcome.php</code>
+//         <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
